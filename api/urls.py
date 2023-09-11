29c43ced5777800a11006d662e33e5e5dd34a85c
VolhaSarkisova <olkavalkiria@gmial.com>
2023-09-11 14:48:04 +0300

    -feat: add mixin

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path, include
 
-from api.views import GoodApiView, all_categories, ArticleGenericView, CategoryMixinGenericView, mixin_categories
+from api.views import GoodApiView, all_categories, ArticleGenericView, CategoryMGenericView
 from .routers import router
 
 urlpatterns = [
     path('users/', include(router.urls)),
     path('goods/', GoodApiView.as_view()),
     path('categories/', all_categories),
-    path('mixin/', CategoryMixinGenericView.as_view()),
-    # path(r'^(?P<language>[a-z]{2})/$', mixin_categories),
-    path('mix/<language>/', mixin_categories),
+    path('mixin/<language>/', CategoryMGenericView.as_view()),
     path('articles/', ArticleGenericView.as_view()),
     path('articles/<int:pk>/', ArticleGenericView.as_view()),
 ]
